0ab5d7fef26888287ab71e065d09fc39c45e4749
maciej.holysz <maciejholysz@gmail.com>
2018-03-18 09:33:16 +0100

    Zadanie 1

--- src/Zadanie1/StandardInputDemo.java
+++ src/Zadanie1/StandardInputDemo.java
@@ -3,8 +3,23 @@ package Zadanie1;
 public class StandardInputDemo {
 
     public static void main(String[] args) {
-	String checkedInputChar = args[0];
+        String checkedInputChar = args[0].toLowerCase();
+        char[] checkedInputArray = checkedInputChar.trim().toCharArray();
+        System.out.print(checkedInputChar + " ");
+        int counter = 0;
+        for (int i = 1; i < args.length; i++) {
+            char[] argsInput = args[i].toLowerCase().toCharArray();
+            for (int j = 0; j < argsInput.length; j++) {
+                if (checkedInputArray[0] == argsInput[j]) {
+                    counter++;
+                }
 
+            }
+
+            System.out.print(args[i] + " ");
+        }
+
+        System.out.println("\n" + counter);
 
     }
 }
